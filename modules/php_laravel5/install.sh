6ac47012930e0d21d1c1fd05828f7659bc5c3765
Dean Howe <dean@millrd.co.uk>
2015-01-20 23:28:27 +0000

    Yes they do.
    Signed-off-by: Dean Howe <dean@millrd.co.uk>

---
+++ modules/php_laravel5/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###############################################################################
+#                                                                             #
+# Laravel 5 - Simplicity is the essence of happiness.                         #
+#                                                                             #
+###############################################################################
+echo "Unfortunately you have not yet arrived."
+
+return 0 || exit 0
